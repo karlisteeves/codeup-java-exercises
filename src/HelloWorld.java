dddd9cc215197ad03371a1323bb42a22fccd9013
Karli Steeves <steeves.karli@gmail.com>
2019-11-20 15:05:47 -0600

    done with exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,29 @@
+public class HelloWorld {
+
+    public static void  main(String[] args){
+//        System.out.println("Hello World!");
+
+    int myFavoriteNumber = 5;
+    System.out.println(myFavoriteNumber);
+
+    String myString = "My name is Karli";
+    System.out.println(myString);
+
+    float myNumber = 3.14f;
+    System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        int x = 4 + 5;
+
+
+        System.out.println(x);
+
+    }
+}

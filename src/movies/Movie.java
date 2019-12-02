9486ecf57fa2d13801c6a76a54cb49e1813c227a
Karli Steeves <steeves.karli@gmail.com>
2019-12-02 14:30:47 -0600

    adding getters and setters

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -2,4 +2,29 @@ package movies;
 
 public class Movie {
 
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+    public String category;
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+
 }
+
+
